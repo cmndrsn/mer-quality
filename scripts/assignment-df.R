47b1bcc97b7167da18f702138b975ece2001837b
Cameron Anderson <andersoc@mcmaster.ca>
2025-08-22 18:29:47 -0400

    fix(assignment-df.R): re-assign datasets
    rework assignments to exclude datasets no longer in evaluation

--- scripts/assignment-df.R
+++ scripts/assignment-df.R
@@ -1,17 +1,16 @@
-set.seed(1)
+set.seed(6)
 # Assign each rater 12 unique datasets ----------------------------
 
-raters <- c("CA", "TE", "JGC", "XH", "VA")
+raters <- sample(c("CA", "TE", "JGC", "XH", "VA"))
 
 assign_dfs <- function(
-    n_df = 21,
-    n_rater_df = 3, # raters per dataframe
-    seed = 1
+    n_df = 20,
+    n_rater_df = 3 # raters per dataframe
+
 ) {
-  set.seed(seed)
   n_rater <- length(raters)
   # store the number of datasets each rater must evalaute
-  n_df_rater <- floor((n_df*n_rater_df)/n_rater) # dataframes per rater
+  n_df_rater <- floor((n_df*n_rater_df)/n_rater) - 1 # dataframes per rater
 
   # create empty list to store dataset assignments
   rating_list <- list()
@@ -19,6 +18,7 @@ assign_dfs <- function(
   message("Each rater evaluates ", n_df_rater, " datasets")
   # represent each dataset 3 times
   assignments <- rep(LETTERS[1:n_df], each = n_rater_df)
+  assignments <- assignments[1:(length(assignments) - 1)]
   # identify each assignment uniquely
   assignments <- paste0(assignments, 1:n_rater_df)
 
@@ -66,7 +66,11 @@ df$rater <- df$rater |>
 
 source('lib/get-datasets.R')
 
+
+
 df$dataset <- factor(df$dataset)
+
+mer_data <- mer_data[!mer_data %in% c("EmoBox (INTERSPEECH)", "NTWICM (pilot)", "RAVDESS")]
 levels(df$dataset) <- mer_data
 
 
@@ -74,24 +78,25 @@ levels(df$dataset) <- mer_data
 
 # get under-represented datasets
 unassigned <- levels(df$dataset)[summary(df$dataset) < 3]
-unassigned <- unassigned[unassigned != "EmoBox (INTERSPEECH)"]
-
 # populate empty list to assign remaining datasets
 assignment_list <- list()
 
 
+tmp1 <- raters
 # loop through remaining datasets that need to be assigned
 for(unassigned_df in unassigned) {
   # sample a rater
-  new_rater <- sample(raters, 1)
+  new_rater <- sample(tmp1, 1)
   # keep resampling until we find a rater who hasn't already been
   # assigned to this dataset
   while(new_rater %in% df[df$dataset == unassigned_df,]$rater) {
-    new_rater <- sample(raters, 1)
+    new_rater <- sample(tmp1, 1)
   }
   new_assignment <- data.frame(rater = new_rater, dataset = unassigned_df)
   # add new assignment to the list of assignments
   assignment_list[[unassigned_df]] <- new_assignment
+  # remove this rater from assignable raters
+  tmp1 <- tmp1[tmp1 != new_rater]
 }
 
 # format newly-assigned datasets
@@ -117,7 +122,6 @@ rater_long <- rep(
 )
 
 # account for mismatch in length of raters and data (not divisible)
-set.seed(1)
 rater_long <- c(rater_long, 
   sample(
   rater_long, 
@@ -183,27 +187,7 @@ for(this_rater in unique(rater_long)) {
 
 df <- dplyr::left_join(df, tiebreaker_df)
 
- # double check assigned tiebreaker actually evaluated dataset
-df <- df |>
-  dplyr::group_by(
-    dataset
-  ) |>
-  dplyr::mutate(check = tiebreaker %in% rater)
-# subset mismatches
-tmp <- df[df$check == FALSE,]
-# replace tiebreaker assignment 
-tmp$tiebreaker <- sample(tmp$rater, 1)
-df <- df[df$check == TRUE,]
-df <- rbind(df, tmp)
-df$check <- NULL
-
-# summarize rater coverage
-summary(df$rater)
-summary(as.factor(df$tiebreaker))
-
-# summarize dataset coverage
-summary(df$dataset)
-
+ezDesign(df, dataset, rater)
 df |> 
   knitr::kable() |>
   writeLines(con = "content/assignment.md")
